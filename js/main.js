b8b76e762bc08479b6e29b2326e02d53abd8dd04
Nathan Ngoyi <ntngo10@gmail.com>
2021-01-26 15:35:21 -0500

    Added page switching to navigation

--- js/main.js
+++ js/main.js
@@ -6,3 +6,40 @@ document.addEventListener('DOMContentLoaded', function() {
         hover: true
     });
 });
+
+const APP = {
+    pages: [],
+    show: new Event('show'),
+    init() {
+        this.pages = document.querySelectorAll('.page');
+        this.pages.forEach(pg => {
+            pg.addEventListener('show', this.pageShown);
+        })
+
+        document.querySelectorAll('.nav-links').forEach(link => {
+            link.addEventListener('click', this.nav);
+        })
+        history.replaceState({}, 'Home', '#home');
+        windows.addEventListener('popstate', this.popping)
+    },
+    nav(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.dataset.target;
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(this.show);
+    },
+    pageShown(ev) {
+        console.log('page', ev.target.id, 'just shown')
+    },
+    popping() {
+        console.log(location.hash, 'popstate event')
+        let hash = location.hash.replace('#', '')
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        document.getElementById(hash).dispatchEvent(this.show);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', APP.init)
